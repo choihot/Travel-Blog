b63167a57747537efefbef0ce0d0c2caf34d10f8
choihot <319054668@qq.com>
2024-06-24 22:04:12 +0800

    initial commit of frontend

---
+++ travel-blog/next.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    i18n: {
+        locales: ["en", "hk", "cn"],
+        defaultLocale: "en",
+    }
+};
+
+export default nextConfig;
---
+++ travel-blog/src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Choi's Travel Blog
+      </h1>
+    </>
+  )
+}
